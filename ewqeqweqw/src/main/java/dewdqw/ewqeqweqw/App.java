561f929113efc7f9ba56819156e8349ef275ac20
163011 <163011@A2931.labinfo.facens.br>
2018-02-22 22:07:54 -0300

    Inicial

---
+++ ewqeqweqw/src/main/java/dewdqw/ewqeqweqw/App.java
@@ -0,0 +1,13 @@
+package dewdqw.ewqeqweqw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
